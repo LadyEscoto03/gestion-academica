27c0bfec4f67da58a4f52d665cecde74899e93b7
Lady Escoto <lady.escoto@uned.cr>
2026-01-13 18:04:54 -0600

    Crear componente y formulario livewire de matrículas

---
+++ app/Livewire/Enrollments/EnrollmentCreate.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Enrollments;
+
+use Livewire\Component;
+
+class EnrollmentCreate extends Component
+{
+    public function render()
+    {
+        return view('livewire.enrollments.enrollment-create');
+    }
+}
