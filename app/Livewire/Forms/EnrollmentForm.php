9d5adcd566c80b8ef29056b0347d8d3ece527042
Lady Escoto <lady.escoto@uned.cr>
2026-01-14 14:58:22 -0600

    Añadir validaciones en el form de matrículas

--- app/Livewire/Forms/EnrollmentForm.php
+++ app/Livewire/Forms/EnrollmentForm.php
@@ -7,5 +7,10 @@ use Livewire\Form;
 
 class EnrollmentForm extends Form
 {
-    //
+    #[Validate('required|numeric|min:4')]
+    public $year = 0;
+    #[Validate('nullable|string|max:255')]
+    public $description = '';
+    #[Validate('required|string|max:255')]
+    public $state = '';
 }
