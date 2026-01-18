c995120b87b6b52d68cd2942b2a6c1a6729ac913
Lady Escoto <lady.escoto@uned.cr>
2026-01-18 13:16:19 -0600

    Crear seeder de categorias de discapacidades

---
+++ database/seeders/DiscapabilityCategorySeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\DisabilityCategory;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DiscapabilityCategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $categories=[
+            'Auditiva',
+            'Sensorial',
+            'Intelectual',
+            'Psicosocial/Mental',
+            'Motora/Física',
+            'Neurológica',
+            'Multidiscapacidad',
+            'Discapacidad del habla y lenguaje',
+            'Discapacidad cognitiva',
+            'Discapacidad del aprendizaje'
+        ];
+
+        foreach ($categories as $category){
+            DisabilityCategory::create([
+                'name'=>$category
+            ]);
+
+        }
+        
+    }
+}
