0411135a849d56a56e56416dbe03352efc2918fa
Lady Escoto <lady.escoto@uned.cr>
2026-01-26 12:58:40 -0600

    Crear componente de editar matricula

---
+++ app/Livewire/Enrollments/EnrollmentUpdate.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Enrollments;
+
+use Livewire\Component;
+
+class EnrollmentUpdate extends Component
+{
+    public function render()
+    {
+        return view('livewire.enrollments.enrollment-update');
+    }
+}
